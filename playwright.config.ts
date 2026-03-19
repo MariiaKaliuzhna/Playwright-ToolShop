7d28d889ee8d97f94e821de08909fe19b1ecbcb2
Mariia Kaliuzhna <comfy51463@gmail.com>
2026-03-20 00:20:43 +0200

    update config file

--- playwright.config.ts
+++ playwright.config.ts
@@ -4,8 +4,8 @@ export default defineConfig({
   testDir: './tests',
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
-  retries: 2,
-  workers: 2,
+  retries: process.env.CI ? 2 : 0,
+  workers: process.env.CI ? 1 : undefined,
   reporter: 'html',
   use: {
     baseURL: 'https://practicesoftwaretesting.com',
